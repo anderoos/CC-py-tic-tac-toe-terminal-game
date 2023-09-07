f2b674b3b3f0b14a7417df034793739d2d0eeef4
Andrew Cheng <138713169+anderoos@users.noreply.github.com>
2023-09-07 18:08:09 -0400

    recreated base framework
    Code now actually works

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Python Terminal Game
-#### Version: 0.1
+#### Version: 0.2
 ## Description
 This is a simple terminal script written in Python that initiates a game of Tic Tac Toe. The user will be asked to play with a second player and match three X's or O's diagonally, vertically or horizontally. First player to match three will win. Game will loop until the player requests not to. 
 
 This code has no license restrictions.
 
-## Upcoming changes
-    * v0.2 - To add win conditions.
-
 ## Version log
-    * v0.1 - Initiated project, has basic functionality, still needs lots of work.
+    * 9/7/23 v0.2 - Simplified program loops and restructured most of the program. 
+                    Added win conditions.
+    
+    * 8/31/23 v0.1 - Initiated project, has basic functionality, still needs lots of work.
--- tictactoe.py
+++ tictactoe.py
@@ -1,206 +1,134 @@
-import time 
-class TicTacToeBoard:
-    # initializes an empty board with empty values
-    # game_state = 0 "game ends"; game_state = 1 "game proceeding"
-    def board_initialize(): 
-        game_state = 1
-        board=[[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
-        print(f"{board[0][0]} | {board[0][1]} | {board[0][2]} ")
-        print("----------")
-        print(f"{board[1][0]} | {board[1][1]} | {board[1][2]} ")
-        print("----------")
-        print(f"{board[2][0]} | {board[2][1]} | {board[2][2]} ")
-        return game_state
-    # resets board to empty values
-    def reset_board():
-        board=[[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
-        print("The board has been reset!")
-    def game_terminate():
-        if game_state == 0:
-            repeat = str(input("Play again?"))
-            if repeat == "Yes":
-                TicTacToeGame.gameinit()
-class TicTacToePlayer(TicTacToeBoard):
-    # Returns board
-    def return_board():
-        print(f"{board[0][0]} | {board[0][1]} | {board[0][2]} ")
-        print("----------")
-        print(f"{board[1][0]} | {board[1][1]} | {board[1][2]} ")
-        print("----------")
-        print(f"{board[2][0]} | {board[2][1]} | {board[2][2]} ")
-    # Player 1 actions
-    # Needs work
-    def p1_move():
-        time.sleep(1)
-        print("Player 1, its your turn!")
-        time.sleep(1)
-        print("Here are your moves:")
-        time.sleep(1)
-        print(list(move for move in moveset.keys()))
-        time.sleep(1)
-        move1 = str(input("Where would you like to put your X? "))
-        if move1 == "top left":
-            if board[0][0] == " ":
-                board[0][0] = "X"
-            else:
-                print("This space is occupied, try again")  # !  Need to add redundancy function
-        elif move1 == "top middle":
-            if board[0][1] == " ":
-                board[0][1] = "X"
-        elif move1 == "top right":
-            if board[0][2] == " ":
-                board[0][2] = "X"
-        elif move1 == "middle left":
-            if board[1][0] == " ":
-                board[1][0] = "X"
-        elif move1 == "middle":
-            if board[1][1] == " ":
-                board[1][1] = "X"
-        elif move1 == "middle right":
-            if board[1][2] == " ":
-                board[1][2] = "X"
-        elif move1 == "bottom left":
-            if board[2][0] == " ":
-                board[2][0] = "X"
-        elif move1 == "bottom middle":
-            if board[2][1] == " ":
-                board[2][1] = "X"
-        elif move1 == "bottom right":
-            if board[2][2] == " ":
-                board[2][2] = "X"
-        else:
-            print("Check your spelling.")
-    def p2_move():
-        time.sleep(1)
-        print("Player 2, its your turn!")
-        time.sleep(1)
-        print("Here are your moves:")
-        time.sleep(1)
-        print(list(move for move in moveset.keys()))
-        time.sleep(1)
-        move1 = str(input("Where would you like to put your O? "))
-        if move1 == "top left":
-            if board[0][0] == " ":
-                board[0][0] = "O"
-            else:
-                print("This space is occupied, try again")  # !  Need to add redundancy function
-        elif move1 == "top middle":
-            if board[0][1] == " ":
-                board[0][1] = "O"
-        elif move1 == "top right":
-            if board[0][2] == " ":
-                board[0][2]== "O"
-        elif move1 == "middle left":
-            if board[1][0] == " ":
-                board[1][0] = "O"
-        elif move1 == "middle":
-            if board[1][1] == " ":
-                board[1][1] = "O"
-        elif move1 == "middle right":
-            if board[1][2] == " ":
-                board[1][2] = "O"
-        elif move1 == "bottom left":
-            if board[2][0] == " ":
-                board[2][0] = "O"
-        elif move1 == "bottom middle":
-            if board[2][1] == " ":
-                board[2][1] = "O"
-        elif move1 == "bottom right":
-            if board[2][2] == " ":
-                board[2][2] = "O"
-        else:
-            print("Check your spelling.")
-class TicTacToeGame(TicTacToePlayer, TicTacToeBoard):
-    def game_duration():
-        print("Great! Lets start!")
-        time.sleep(1)
-        print("Setting up board... \n")
-        time.sleep(1)
-        TicTacToeBoard.board_initialize()
-        while game_state == 1:
-            TicTacToePlayer.p1_move()
-            TicTacToePlayer.p2_move()
-    def check_win_conditions():
-        # All horizontal options
-        if (board[0][0] == "X" and board[0][1] == "X" and board[0][2] == "X") or (board[0][0] == "O" and board[0][1] == "O" and board[0][2] == "O"):
-            print("Looks like we have a winner!")
-            game_state = 0
-            game_terminate()
-        elif (board[1][0] == "X" and board[1][1] == "X" and board[1][2] == "X") or (board[1][0] == "O" and board[1][1] == "O" and board[1][2] == "O"):
-            print("Looks like we have a winner!")
-            game_state = 0
-            game_terminate()
-        elif (board[2][0] == "X" and board[2][1] == "X" and board[2][2] == "X") or (board[2][0] == "O" and board[2][1] == "O" and board[2][2] == "O"):
-            print("Looks like we have a winner!")
-            game_state = 0
-            game_terminate()
-        else:
-            pass
-        # All vertical options
-        if (board[0][0] == "X" and board[1][0] == "X" and board[2][0] == "X") or (board[0][0] == "O" and board[1][0] == "O" and board[2][0] == "O"):
-            print("Looks like we have a winner!")
-            game_terminate()
-        elif (board[0][1] == "X" and board[1][1] == "X" and board[2][1] == "X") or (board[0][1] == "O" and board[1][1] == "O" and board[2][1] == "O"):
-            print("Looks like we have a winner!")
-            game_terminate()
-        elif (board[0][2] == "X" and board[1][2] == "X" and board[2][2] == "X") or (board[0][2] == "O" and board[1][2] == "O" and board[2][2] == "O"):
-            print("Looks like we have a winner!")
-            game_state = 0
-            game_terminate()
-        else:
-            pass
-        # All diagonal options
-        if (board[0][0] == "X" and board[1][1] == "X" and board[2][2] == "X") or (board[0][0] == "O" and board[1][1] == "O" and board[2][2] == "O"):
-            print("Looks like we have a winner!")
-            game_state = 0
-            game_terminate()
-        elif (board[0][2] == "X" and board[1][1] == "X" and board[2][0] == "X") or (board[0][2] == "O" and board[1][1] == "O" and board[2][0] == "O"):
-            print("Looks like we have a winner!")
-            game_state = 0
-            game_terminate()
-        else:
-            pass
+import time
+import sys
 
-
-        
-board=[[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
-moveset = {
-"top right": board[0][0],
-"top middle": board[0][1],
-"top left": board[0][2],
-"middle left": board[1][0],
-"middle": board[1][1],
-"middle right": board[1][2],
-"bottom left": board[2][0],
-"bottom middle": board[2][1],
-"bottom right": board[2][2]
-}
-
-print("Welcome! Lets play some Tic Tac Toe!")
-print("You'll need two playeres for this game.")
-ready_status = str(input("Are you ready to continue? "))
-if ready_status == "Yes":
-    TicTacToeGame.gameinit()
+board_status = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+moveset_disp = {
+        0: "top right",
+        1: "top middle",
+        2: "top left",
+        3: "middle left",
+        4: "middle",
+        5: "middle right",
+        6: "bottom left",
+        7: "bottom middle",
+        8: "bottom right"}
     
-    TicTacToePlayer.p1_move()
+def print_board(board):
+    print(f"{board[0]} | {board[1]} | {board[2]} ")
+    print("----------")
+    print(f"{board[3]} | {board[4]} | {board[5]} ")
+    print("----------")
+    print(f"{board[6]} | {board[7]} | {board[8]} ")
+
+# def playagain():
+#         ready_status = str(input("Play again? "))
+#         if ready_status == "no" or ready_status == "No":
+#             print("Terminating game...")
+#         elif ready_status == "yes" or ready_status == "Yes":
+#             game_start()
+
+def game_menu():
+    print("Welcome! Ready to play some tic tac toe?")
+    print("You'll need two players for this game.")
+    ready_status = str(input("Are you ready to continue? "))
+    if ready_status == "no" or ready_status == "No":
+        print("See you later!")
+        sys.exit()
+    elif ready_status == "yes" or ready_status =="Yes":
+        game_start()
+    else:
+        print("So is that a yes or no?")
+
+def check_win_conditions():
+    # Declare winning combinations
+    winning_combos = [(0, 1, 2), (3, 4, 5), (6, 7, 8),
+                    (0, 3, 6), (1, 4, 7), (2, 5, 8),
+                    (0, 4, 8), (2, 4, 6)]
+    # Checking for winning combinations
+    for winning_set in winning_combos:
+            if board_status[winning_set[0]] == board_status[winning_set[1]] == board_status[winning_set[2]] == "X":
+                print("Player X Wins!")
+                time.sleep(1)
+                print("Player X Wins!")
+                time.sleep(1)
+                print("Player X Wins!")
+                time.sleep(1)
+                # Reset game status, ask players to play again
+                # playagain()
+                return True
+            elif " " not in board_status:
+                print("Tied!")
+                time.sleep(1)
+                # playagain()
+                return True
+            elif board_status[winning_set[0]] == board_status[winning_set[1]] == board_status[winning_set[2]] == "O":
+                print("Player O Wins!")
+                time.sleep(1)
+                print("Player O Wins!")
+                time.sleep(1)
+                print("Player O Wins!")
+                time.sleep(1)
+                # Reset game status, ask players to play again
+                # playagain()
+                return True
+            else:
+                return False
+
+def playerX():
     time.sleep(1)
-    TicTacToePlayer.return_board()
-    TicTacToePlayer.p2_move()
+    print("Player X, its your turn!")
     time.sleep(1)
-    TicTacToePlayer.return_board()
-    TicTacToePlayer.p1_move()
+    print("Here are your moves:")
     time.sleep(1)
-    TicTacToePlayer.return_board()
-    TicTacToePlayer.p2_move()
+    print(list(move for move in moveset_disp.items()))
+    moveX = int(input("Place your X: "))
+    if board_status[moveX] == " ":
+        board_status[moveX] = "X"
+        print_board(board_status)
+    else:
+        print("That space seems occupied. Try again.")
+        moveX = int(input("Place your X: "))
+        if board_status[moveX] == " ":
+            board_status[moveX] = "X"
+            print_board(board_status)
+        else:
+            print("That space seems occupied. Try again.")
+            moveX = int(input("Place your X: "))
+
+def playerO():
     time.sleep(1)
-    TicTacToePlayer.return_board()
-    TicTacToePlayer.p1_move()
+    print("Player O, its your turn!")
     time.sleep(1)
-    TicTacToePlayer.return_board()
-    TicTacToePlayer.p2_move()
+    print("Here are your moves:")
     time.sleep(1)
-    TicTacToePlayer.return_board()
-    TicTacToeGame.check_win_conditions()
-else:
-    print("See you later!")
+    print(list(move for move in moveset_disp.items()))
+    moveO = int(input("Place your O: "))
+    if board_status[moveO] == " ":
+        board_status[moveO] = "O"
+        print_board(board_status)
+    else:
+        print("That space seems occupied. Try again.")
+        moveO = int(input("Place your O: "))
+        if board_status[moveO] == " ":
+            board_status[moveO] = "O"
+            print_board(board_status)
+        else:
+            print("That space seems occupied. Try again.")
+            moveO = int(input("Place your O: "))
+
+def game_start():
+    while check_win_conditions() == False:
+        playerX()
+        check_win_conditions()
+        playerO()
+        check_win_conditions()
+
+def game_restart():
+    board_status = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    return board_status
+game_menu()
+
+
+
+
 
